27237620cbff461f7780cd8eb488bd437c8627d2
Salah Eddine Blilit <blilitsalaheddine@gmail.com>
2025-03-12 02:37:22 +0000

    navbar and pages routing setup

--- Platforme_formation_continue/resources/views/layouts/app.blade.php
+++ Platforme_formation_continue/resources/views/layouts/app.blade.php
@@ -29,7 +29,12 @@
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+            @if(isset($slot))
+                    {{ $slot }}  {{-- Blade component content --}}
+                @else
+                    @yield('content')  {{-- Normal Blade views --}}
+                @endif
+
             </main>
         </div>
     </body>
