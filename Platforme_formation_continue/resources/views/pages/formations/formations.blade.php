ff8871dd361769c9a70b1dc10ebf364b2c7110c5
Salah Eddine Blilit <blilitsalaheddine@gmail.com>
2025-03-14 10:54:03 +0000

    Home page frontend setup

--- Platforme_formation_continue/resources/views/pages/formations/formations.blade.php
+++ Platforme_formation_continue/resources/views/pages/formations/formations.blade.php
@@ -1,11 +1,7 @@
-@extends('layouts.app');
-@section('header')
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        {{ __('Formations') }}
-    </h2>
-@endsection
+@extends('layouts.app')
+
 @section('content')
-    <div class="py-12">
+<div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
@@ -13,7 +9,7 @@
                         <x-application-logo class="block h-12 w-auto fill-current text-gray-600" />
                     </div>
                     <div class="mt-8 text-2xl">
-                        {{ __('Formations page') }}
+                        {{ __('Formations Page') }}
                     </div>
                     <div class="mt-6 text-gray-500">
                         {{ __('Cette plateforme a été créée dans le cadre du projet de fin d\'études de la formation de développeur web et web mobile de l\'école O\'clock.') }}
