8f324e67d98aa9f02faa5ad05d106b4b3c9a4520
MohamedBenlmouden <benlmouden20mohamed01@gmail.com>
2025-03-16 17:04:07 +0100

    fixed seeders

--- Platforme_formation_continue/database/seeders/FormationSeeder.php
+++ Platforme_formation_continue/database/seeders/FormationSeeder.php
@@ -31,7 +31,7 @@ class FormationSeeder extends Seeder
             [
                 'nom' => 'Management',
                 'description' => 'Management stratégique, organisationnel, opérationnel, et des équipes.',
-                'domaine_id' => 2, // Modified domaine_id to 2
+                'domaine_id' => 1, // Modified domaine_id to 2
                 'image' => 'management.jpg',
                 'etablissement_id' => 1,
                 'sub_titles' => json_encode([
@@ -44,7 +44,7 @@ class FormationSeeder extends Seeder
             [
                 'nom' => 'Finances et gestion financière',
                 'description' => 'Procédure budgétaire et comptable, gestion et stratégie financière, fiscalité.',
-                'domaine_id' => 3, // Modified domaine_id to 3
+                'domaine_id' => 1, // Modified domaine_id to 3
                 'image' => 'finances_gestion.jpg',
                 'etablissement_id' => 1,
                 'sub_titles' => json_encode([
@@ -56,77 +56,77 @@ class FormationSeeder extends Seeder
             [
                 'nom' => 'Communication',
                 'description' => 'Stratégie de communication, techniques et outils de communication.',
-                'domaine_id' => 4, // Modified domaine_id to 4
+                'domaine_id' => 1, // Modified domaine_id to 4
                 'image' => 'communication.jpg',
                 'etablissement_id' => 1,
             ],
             [
                 'nom' => 'Informatique et systèmes d’information',
                 'description' => 'Architecture et administration des systèmes d’information, réseaux et télécommunication.',
-                'domaine_id' => 5, // Modified domaine_id to 5
+                'domaine_id' => 1, // Modified domaine_id to 5
                 'image' => 'informatique_systemes.jpg',
                 'etablissement_id' => 1,
             ],
             [
                 'nom' => 'Sécurité - Hygiène',
                 'description' => 'Prévention et opération d’incendie, sécurité dans la ville, gestion des risques technologiques et naturels.',
-                'domaine_id' => 3,
+                'domaine_id' => 1,
                 'image' => 'securite_hygiene.jpg',
                 'etablissement_id' => 1,
             ],
             [
                 'nom' => 'Commerce et marketing',
                 'description' => 'Stratégie marketing, technique commerciale et de vente.',
-                'domaine_id' => 7,
+                'domaine_id' => 1,
                 'image' => 'commerce_marketing.jpg',
                 'etablissement_id' => 1,
             ],
             [
                 'nom' => 'Qualité',
                 'description' => 'Amélioration de la qualité, assurance qualité, système de management qualité.',
-                'domaine_id' => 8,
+                'domaine_id' => 1,
                 'image' => 'qualite.jpg',
                 'etablissement_id' => 1,
             ],
             [
                 'nom' => 'Technique',
                 'description' => 'Gestion de la production, gestion et maîtrise de l’énergie, gestion de la maintenance.',
-                'domaine_id' => 9,
+                'domaine_id' => 1,
                 'image' => 'technique.jpg',
                 'etablissement_id' => 1,
             ],
             [
                 'nom' => 'Logistique',
                 'description' => 'Gestion des approvisionnements, gestion des stocks, gestion des magasins.',
-                'domaine_id' => 10,
+                'domaine_id' => 1,
                 'image' => 'logistique.jpg',
                 'etablissement_id' => 1,
             ],
             [
                 'nom' => 'Environnement',
                 'description' => 'Politique d’environnement, gestion des ressources, traitement des eaux usées et gestion des déchets.',
-                'domaine_id' => 11,
+                'domaine_id' => 1,
                 'image' => 'environnement.jpg',
                 'etablissement_id' => 1,
             ],
             [
                 'nom' => 'Affaires juridiques',
                 'description' => 'Mode et gestion des services publics, achats publics, contrats et assurances.',
-                'domaine_id' => 12,
+                'domaine_id' => 1,
                 'image' => 'affaires_juridiques.jpg',
                 'etablissement_id' => 1,
             ],
             [
                 'nom' => 'Langues',
                 'description' => 'Aspect général des langues, aspect technique et commercial des langues.',
-                'domaine_id' => 13,
+                'domaine_id' => 1,
                 'image' => 'langues.jpg',
                 'etablissement_id' => 1,
             ],
             [
                 'nom' => 'Santé',
                 'description' => 'Soins médicaux, secourisme, biomédical, soins dentaires et analyses médicales.',
-                'domaine_id' => 14,
+                'domaine_id' => 1,
                 'image' => 'sante.jpg',
                 'etablissement_id' => 1,
             ],
