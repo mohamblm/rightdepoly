6de0271c95763d2a5d5c5cc98f4cfa77c932eb8b
Salah Eddine Blilit <blilitsalaheddine@gmail.com>
2025-03-26 12:49:17 +0000

    fix js issues related to turbo loading

--- Platforme_formation_continue/resources/views/pages/home/popularcourses.blade.php
+++ Platforme_formation_continue/resources/views/pages/home/popularcourses.blade.php
@@ -59,28 +59,36 @@
 @push('scripts')
 <script>
 
-        // Initialize Swiper for Popular Courses with unique selectors
-        const swiperPopular = new Swiper('.mySwiper-popular-courses', {
-            slidesPerView: 1,
-            spaceBetween: 20,
-            breakpoints: {
-                // when window width is >= 640px
-                640: {
-                    slidesPerView: 2,
+        // Function to initialize Swiper for Popular Courses
+        function initializeSwiperPopular() {
+            const swiperPopular = new Swiper('.mySwiper-popular-courses', {
+                slidesPerView: 1,
+                spaceBetween: 20,
+                breakpoints: {
+                    // when window width is >= 640px
+                    640: {
+                        slidesPerView: 2,
+                    },
+                    // when window width is >= 1024px
+                    1024: {
+                        slidesPerView: 3,
+                    },
+                    // when window width is >= 1280px
+                    1280: {
+                        slidesPerView: 4,
+                    }
                 },
-                // when window width is >= 1024px
-                1024: {
-                    slidesPerView: 3,
+                autoplay: {
+                    delay: 5000,
                 },
-                // when window width is >= 1280px
-                1280: {
-                    slidesPerView: 4,
-                }
-            },
-            autoplay: {
-                delay: 5000,
-            },
-        });
+            });
+        }
+
+        // Initialize Swiper on page load
+        initializeSwiperPopular();
+
+        // Reinitialize Swiper after Turbo renders
+        document.addEventListener('turbo:load', initializeSwiperPopular);
     
 </script>
 @endpush
